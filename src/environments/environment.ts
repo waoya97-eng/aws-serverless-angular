532479f131226e9d06308817dea7d85475b511cb
近藤直哉 <waoya97@gmail.com>
2026-09-02 14:28:50 +0900

    設定変更

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   cognito: {
-    userPoolId: 'ap-northeast-1_XXXXXXXXX',
-    userPoolClientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+    userPoolId: 'us-east-1_xD964pR7l',
+    userPoolClientId: '36lfojedjnp5pv4024om4m5av9',
   },
-  apiEndpoint: 'https://xxxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com/prod',
-  cloudfrontUrl: 'https://xxxxxxxxxxxx.cloudfront.net',
+  apiEndpoint: 'https://jaxckjkhj8.execute-api.us-east-1.amazonaws.com/prod',
+  cloudfrontUrl: 'https:d3j4f2xij1apf8.cloudfront.net',
 };
